5b7d3f18b7d21ab85fcea0a9601dc817da416a50
Jesse Peixoto <jessepeixoto@gmail.com>
2022-03-10 16:07:40 +0100

    reuse transform_data

---
+++ transform_data.py
@@ -0,0 +1,14 @@
+import nltk
+
+special_characters = ['!', '#', '$', '%', '&', '@', '[', ']', ']', '_', '-', '®', '?', ':', '(', ')', '™', '/',
+                      '\'', ',', '+']
+
+def transform_data(data):
+    tokens = nltk.word_tokenize(data)
+    tokens = [token.lower() for token in tokens]
+    tokens = [token for token in tokens if (token not in nltk.corpus.stopwords.words('english'))]
+    tokens = [nltk.stem.snowball.SnowballStemmer("english").stem(token) for token in tokens]
+    data = ' '.join(tokens)
+    for special_character in special_characters:
+        data = data.replace(special_character, ' ')
+    return data
--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -4,23 +4,25 @@ import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+import pandas as pd
+
+from transform_data import transform_data
+
 
 # Directory for product data
 directory = r'/workspace/search_with_machine_learning_course/data/pruned_products/'
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 general = parser.add_argument_group("general")
-general.add_argument("--input", default=directory,  help="The directory containing product data")
+general.add_argument("--input", default=directory, help="The directory containing product data")
 general.add_argument("--output", default="/workspace/datasets/fasttext/output.fasttext", help="the file to output to")
 
 # Consuming all of the product data will take over an hour! But we still want to be able to obtain a representative sample.
-general.add_argument("--sample_rate", default=1.0, type=float, help="The rate at which to sample input (default is 1.0)")
+general.add_argument("--sample_rate", default=1.0, type=float,
+                     help="The rate at which to sample input (default is 1.0)")
 
-# IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
-general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
+general.add_argument("--min_products", default=0, type=int,
+                     help="The minimum number of products per category (default is 0).")
 
 args = parser.parse_args()
 output_file = args.output
@@ -35,6 +37,8 @@ if args.input:
 min_products = args.min_products
 sample_rate = args.sample_rate
 
+rows = []
+
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
     for filename in os.listdir(directory):
@@ -48,11 +52,22 @@ with open(output_file, 'w') as output:
                     continue
                 # Check to make sure category name is valid
                 if (child.find('name') is not None and child.find('name').text is not None and
-                    child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
-                    child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
-                      # Choose last element in categoryPath as the leaf categoryId
-                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                      # Replace newline chars with spaces so fastText doesn't complain
-                      name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
+                        child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
+                        child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
+                    catId = child.find('categoryPath')
 
+                    # Choose last element in categoryPath as the leaf categoryId
+                    if [len(child.find('categoryPath'))][0] > 3:
+                        cat = child.find('categoryPath')[2][0].text
+                    elif [len(child.find('categoryPath'))][0] > 2:
+                        cat = child.find('categoryPath')[1][0].text
+                    else:
+                        cat = child.find('categoryPath')[0][0].text
+                    # Replace newline chars with spaces so fastText doesn't complain
+                    name = child.find('name').text.replace('\n', ' ')
+                    rows.append({"cat": cat, "name": transform_data(name)})
+
+    df = pd.DataFrame(rows, columns=["cat", "name"])
+    dfFiltered = df.groupby(['cat']).filter(lambda x: len(x) >= min_products)
+    dfFiltered['output'] = '__label__' + dfFiltered['cat'] + ' ' + dfFiltered['name']
+    dfFiltered.output.to_csv(output, header=False, index=False, sep='\t')
